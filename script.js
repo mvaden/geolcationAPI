beac2123dc2d51b59612df65227fa5e8ebda2f1d
Matthew Vaden <matthewvaden@gmail.com>
2019-03-11 07:48:54 -0400

    Initial commit.

---
+++ script.js
@@ -0,0 +1,19 @@
+const geo = navigator.geolocation;
+
+function getLocation() {
+    if (geo) {
+        geo.getCurrentPosition(showLocation, error);
+        console.log("Geolocation is supported by this browser.");
+    } else {
+        console.info("Geolocation is not supported by this browser.")
+    }
+}
+
+function showLocation(p) {
+    let s = document.getElementById('cords');
+    s.innerHTML = "<br>Latitude: " + p.coords.latitude + "<br>Longitude: " + p.coords.longitude;
+}
+
+function error() {
+    console.error("Error in retriving location.")
+}
